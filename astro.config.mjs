ccef1055f2fcec5af233fa23d99335a7bf04440e
Alex <agutie22@calstatela.edu>
2026-05-27 08:52:58 -0700

    feat: initial commit with mobile layout updates, pnpm lock, and github actions deploy

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://agutie22.github.io',
+  base: '/el-patron-restaurant',
+});
